8e991c5014daec0ab1313b6011b96817057c0d3f
Gayan Kodithuwakku <gayan.lakshan9@gmail.com>
2022-02-10 23:45:32 +0530

    import createPosts

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import {getPosts} from '../controllers/postsController.js';
+import {getPosts, createPosts} from '../controllers/postsController.js';
 
 const router = express.Router();
 
 //localhost:5000/posts
 
 router.get('/', getPosts)
+router.post('/', createPosts)
 
 export default router;
